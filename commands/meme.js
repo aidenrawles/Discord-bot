357ebd1d7f2414318c23a15f72dab79eb571b823
Aiden Rawles <aidenrawles@Aidens-MBP-2.lan>
2022-05-09 00:30:15 +0800

    meme command commit

---
+++ commands/meme.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = {
+    name: 'meme',
+    description: 'Replies with a meme!',
+    execute(message, args){
+        axios.get(`https://meme-api.herokuapp.com/gimme`)
+        .then((res) => {
+            let memeIndex = (res.data.preview).length - 1;
+            let meme = res.data.preview[memeIndex];
+            message.reply(meme);
+            console.log(`Displaying meme of ${meme}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+};
