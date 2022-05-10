edc2e0da6686f7052e2a5c0f51d24b2911216a90
Aiden Rawles <aidenrawles@Aidens-MacBook-Pro-2.local>
2022-05-11 00:42:57 +0700

    crypto currency feature

--- commands/crypto.js
+++ commands/crypto.js
@@ -6,17 +6,25 @@ module.exports = {
     name: 'crypto',
     description: 'Replies with the price of tagged crypto!',
     execute(message, args) {
-        const tag = args[0]
-        axios.get(`https://rest.coinapi.io/v1/assets/${tag}`, {
+        const currency = args[1];
+        const tag = args[0];
+        axios.get(`https://rest.coinapi.io/v1/exchangerate/${tag}`, {
             headers: {
                 'X-CoinAPI-KEY':`${process.env.COIN_API_KEY}`,
             },
         })
         .then((res) => {
-            let price = +(Math.round(res.data[0].price_usd + "e+2")  + "e-2");
+            const arr = res.data.rates;
+            const index = arr.findIndex(curr => curr.asset_id_quote == currency);
+            let price = +(Math.round(arr[index].rate + "e+2")  + "e-2");
             const newEmbed = new MessageEmbed()
-            .setTitle(`The price of ${res.data[0].asset_id} is $${price}`)
+            .setTitle(`The price of ${res.data.asset_id_base}/USD is $${price}`)
             .setColor('#0099ff');
+
+            if (args[1] !== undefined) {
+                newEmbed.setTitle(`The price of ${res.data.asset_id_base}/${currency} is $${price}`);
+            }
+
             message.reply({ embeds: [newEmbed] });
             console.log('Displaying Crypto Price!');
         })
